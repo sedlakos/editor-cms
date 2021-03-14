b7f3be40968b8ab61d08da6ce168669ffc7d5558
Antonin Sajboch <tonda@Antonins-MacBook-Pro.local>
2021-03-14 17:57:07 +0100

    no-message

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ export function JEBAT(){
     console.log("JEBU")
 }
 
-window.initEditor = initEditor
+window.initGlobalEditor = initEditor
 // initEditor(document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
