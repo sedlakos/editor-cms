bce26348131a771554f1b767f303b3a49576cc26
Antonin Sajboch <tonda@Antonins-MacBook-Pro.local>
2021-03-14 16:12:35 +0100

    no-message

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@react-page/editor/lib/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+export function initEditor(element: Element | DocumentFragment | null) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        element
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
