15c2fbfdf4dbce169b9a5f3bb22c0719d80b2994
Antonin Sajboch <tonda@Antonins-MacBook-Pro.local>
2021-03-14 16:53:17 +0100

    no-message

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@react-page/editor/lib/index.css';
 
-
-export function initEditor(element: any) {
+export default function initEditor(element) {
     ReactDOM.render(
         <React.StrictMode>
             <App />
@@ -15,6 +14,8 @@ export function initEditor(element: any) {
     );
 }
 
+window.initEditor = initEditor
+// initEditor(document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
